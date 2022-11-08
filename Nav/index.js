0ec836fc2cbe5fb0b4642c8be6c1d30fb5846031
Mace <maciek.kierszka@ict-scouts.ch>
2022-11-08 17:41:32 +0100

    Major update 1 html-css-js home-projects

---
+++ Nav/index.js
@@ -0,0 +1,22 @@
+document.write(`
+    <body>
+<style></style>
+<div class="navWhole">
+    <div class="line1" id="line1"></div>
+    <div class="line2" id="line2"></div>
+    <div class="grid-container">
+        <div class="grid-item" id="grid1" onmouseover="onhover(id)"></div>
+        <div class="grid-item" id="grid2" onmouseover="onhover(id)"></div>
+        <div class="grid-item" id="grid3" onmouseover="onhover(id)"></div>
+        <div class="grid-item" id="grid4" onmouseover="onhover(id)"></div>
+    </div>
+    <div class="navHome" id="navHome" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="../index.html">Home</a></div>
+    <div class="navAbout" id="navAbout" onmouseover="onhover(id)"  ><a style="text-decoration: none; color: antiquewhite" href="../projects/index.html">Projekte</a></div>
+    <div class="navTemp1" id="navTemp1" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="../#">Temp</a></div>
+    <div class="navTemp2" id="navTemp2" onmouseover="onhover(id)"><a style="text-decoration: none; color: antiquewhite" href="../#">About</a></div>
+
+</div>
+</body>
+<script src="../index.js"></script>
+`)
+
---
+++ index.js
@@ -0,0 +1,109 @@
+var navHome = document.getElementById("navHome");
+var navAbout = document.getElementById("navAbout");
+var navTemp1 = document.getElementById("navTemp1");
+var navTemp2 = document.getElementById("navTemp2");
+var line1 = document.getElementById("line1")
+var line2 = document.getElementById("line2")
+var BMenu = document.getElementById("BMenu")
+var BMLine1 = document.getElementById("BMLine1")
+var BMLine2 = document.getElementById("BMLine2")
+var BMLine3 = document.getElementById("BMLine3")
+
+var i = 1
+function onhover(id) {
+    i += 1
+    var data = id
+    if (i == 2) {
+    var data = id
+    var marginLeft;
+    console.log(data)
+    if (data == "navHome") {
+        marginLeft = "51.5vw"
+    } else if ( data == "navAbout") {
+        marginLeft = "61.5vw";
+    } else if (data == "navTemp1") {
+        marginLeft = "71.5vw";
+    } else if (data == "navTemp2"){
+        marginLeft = "81.5vw";
+    }
+    line1.style.marginLeft = marginLeft
+    line2.style.marginLeft = marginLeft
+    line1.style.display = "flex";
+    line2.style.display = "flex";
+    line1.style.animation = "lineAppear1 1s";
+    line2.style.animation = "lineAppear2 1s";
+    return;
+        }
+    if (data == "grid1" && line1.style.marginLeft == "51.5vw"){
+        line1.style.marginLeft = "61.5vw"
+        line1.style.animation = "lineshiftRight1 1s"
+        line2.style.marginLeft = "61.5vw"
+        line2.style.animation = "lineshiftRight1 1s"
+    } else if (data == "grid1" && line1.style.marginLeft == "61.5vw") {
+        line1.style.marginLeft = "51.5vw"
+        line1.style.animation = "lineshiftLeft1 1s"
+        line2.style.marginLeft = "51.5vw"
+        line2.style.animation = "lineshiftLeft1 1s"
+    } else if (data == "grid2" && line1.style.marginLeft == "61.5vw") {
+        line1.style.marginLeft = "71.5vw"
+        line1.style.animation = "lineshiftRight2 1s"
+        line2.style.marginLeft = "71.5vw"
+        line2.style.animation = "lineshiftRight2 1s"
+    } else if (data == "grid2" && line1.style.marginLeft == "71.5vw") {
+        line1.style.marginLeft = "61.5vw"
+        line1.style.animation = "lineshiftLeft2 1s"
+        line2.style.marginLeft = "61.5vw"
+        line2.style.animation = "lineshiftLeft2 1s"
+    } else if (data == "grid3" && line1.style.marginLeft == "71.5vw") {
+        line1.style.marginLeft = "81.5vw"
+        line1.style.animation = "lineshiftRight3 1s"
+        line2.style.marginLeft = "81.5vw"
+        line2.style.animation = "lineshiftRight3 1s"
+    } else if (data == "grid3" && line1.style.marginLeft == "71.5vw") {
+        line1.style.marginLeft = "81.5vw"
+        line1.style.animation = "lineshiftLeft3 1s"
+        line2.style.marginLeft = "81.5vw"
+        line2.style.animation = "lineshiftLeft3 1s"
+    } else if (data == "grid3" && line1.style.marginLeft == "81.5vw") {
+        line1.style.marginLeft = "71.5vw"
+        line1.style.animation = "lineshiftLeft4 1s"
+        line2.style.marginLeft = "71.5vw"
+        line2.style.animation = "lineshiftLeft4 1s"
+    }  else if (data == "grid1" && line1.style.marginLeft == "81.5vw") {
+        line1.style.marginLeft = "51.5vw"
+        line1.style.animation = "lineshiftLeft5 1s"
+        line2.style.marginLeft = "51.5vw"
+        line2.style.animation = "lineshiftLeft5 1s"
+    }  else if (data == "grid1" && line1.style.marginLeft == "71.5vw") {
+        line1.style.marginLeft = "51.5vw"
+        line1.style.animation = "lineshiftLeft6 1s"
+        line2.style.marginLeft = "51.5vw"
+        line2.style.animation = "lineshiftLeft6 1s"
+    }  else if (data == "grid2" && line1.style.marginLeft == "81.5vw") {
+        line1.style.marginLeft = "61.5vw"
+        line1.style.animation = "lineshiftLeft7 1s"
+        line2.style.marginLeft = "61.5vw"
+        line2.style.animation = "lineshiftLeft7 1s"
+    } else if (data == "grid2" && line1.style.marginLeft == "51.5vw") {
+        line1.style.marginLeft = "61.5vw"
+        line1.style.animation = "lineshiftLeft8 1s"
+        line2.style.marginLeft = "61.5vw"
+        line2.style.animation = "lineshiftLeft8 1s"
+    } else if (data == "grid3" && line1.style.marginLeft == "51.5vw") {
+        line1.style.marginLeft = "81.5vw"
+        line1.style.animation = "lineshiftLeft9 1s"
+        line2.style.marginLeft = "81.5vw"
+        line2.style.animation = "lineshiftLeft9 1s"
+    } else if (data == "grid3" && line1.style.marginLeft == "61.5vw") {
+        line1.style.marginLeft = "81.5vw"
+        line1.style.animation = "lineshiftLeft10 1s"
+        line2.style.marginLeft = "81.5vw"
+        line2.style.animation = "lineshiftLeft10 1s"
+    } else if (data == "grid3" && line1.style.marginLeft == "71.5vw") {
+        line1.style.marginLeft == "81.5vw"
+        line1.style.animation = "lineshiftRight3 1s"
+        line2.style.marginLeft == "81.5vw"
+        line2.style.animation = "lineshiftRight3 1s"
+    }
+}
+
