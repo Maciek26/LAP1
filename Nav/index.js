1927a836377fdd2f5f73f5ee536cd2b849ecfad3
Mace <maciek.kierszka@ict-scouts.ch>
2022-11-10 18:28:06 +0100

    major update 2

--- Nav/index.js
+++ Nav/index.js
@@ -10,8 +10,8 @@ document.write(`
         <div class="grid-item" id="grid3" onmouseover="onhover(id)"></div>
         <div class="grid-item" id="grid4" onmouseover="onhover(id)"></div>
     </div>
-    <div class="navHome" id="navHome" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="index">Home</a></div>
-    <div class="navAbout" id="navAbout" onmouseover="onhover(id)"  ><a style="text-decoration: none; color: antiquewhite" href="projects/index">Projekte</a></div>
+    <div class="navHome" id="navHome" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="mace-center.me/LAP1">Home</a></div>
+    <div class="navAbout" id="navAbout" onmouseover="onhover(id)"  ><a style="text-decoration: none; color: antiquewhite" href="mace-center.me/LAP1/projects">Projekte</a></div>
     <div class="navTemp1" id="navTemp1" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="../#">Temp</a></div>
     <div class="navTemp2" id="navTemp2" onmouseover="onhover(id)"><a style="text-decoration: none; color: antiquewhite" href="../#">About</a></div>
 
--- projects/index.js
+++ projects/index.js
@@ -1,26 +1,79 @@
 var hoverDivCamera1 = document.getElementById("hoverDivCam1")
+var hoverDivPanel = document.getElementById("hoverDivPanel")
+
 var hoverBubbleCam1 = document.getElementById("hoverBubbleCam1")
 var hoverBubbleCam2 = document.getElementById("hoverBubbleCam2")
 var hoverBubbleCam3 = document.getElementById("hoverBubbleCam3")
 var hoverBubbleArt = document.getElementById("hoverBubbleArt")
-var artIconImage = document.getElementById("artIcon")
-var cameraIcon = document.getElementById("camIcon")
+var hoverBubbleChip = document.getElementById("hoverBubbleChip")
+var hoverBubblePanel = document.getElementById("hoverBubblePanel")
 
+var cameraIcon = document.getElementById("camIcon")
+var artIconImage = document.getElementById("artIcon")
+var chipIconImage = document.getElementById("chipIcon")
+var panelIconImage = document.getElementById("panelIcon")
+var webIconImage = document.getElementById("webIcon")
+
+var group1 = document.getElementById("group1")
+var group2 = document.getElementById("group2")
+var group3 = document.getElementById("group3")
+var group4 = document.getElementById("group4")
+i = 2
+
+function scrollDown(id){
+    var data = id
+    if (data == "hoverBubbleCam1") {
+        group1.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
+    } else if (data == "hoverBubbleCam2") {
+        group2.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
+    } else if (data == "hoverBubbleCam3") {
+        group3.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
+    }//else if (data == "group4") {
+      //  group4.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
+    //}
+}
 
 function hoverDiv(id) {
     var data = id
     if (data == "camIcon") {
+        webIconImage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+        panelIconImage.style.animation = "";
+        panelIconImage.style.backgroundPositionY = "2vw"
+        hoverBubblePanel.style.display = "none"
+        hoverBubblePanel.style.animation = "";
         hoverBubbleCam1.style.display = "block";
         hoverBubbleCam1.style.animation = "bubbleAppear 1.2s"
         hoverBubbleCam2.style.display = "block";
         hoverBubbleCam2.style.animation = "bubbleAppear2 1.2s"
         hoverBubbleCam3.style.display = "block";
         hoverBubbleCam3.style.animation = "bubbleAppear3 1.2s"
+        hoverDivPanel.style.display = "none"
+        hoverBubbleCam3.style.zIndex = "20"
     } else if (data == "artIcon") {
+        webIconImage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
         artIconImage.style.animation = "iconDown 1.2s";
         artIconImage.style.backgroundPositionY = "4.25vw"
         hoverBubbleArt.style.display = "block";
-        hoverBubbleArt.style.animation = "bubbleAppearArt 1.2s"
+        hoverBubbleArt.style.animation = "bubbleAppearArtChip 1.2s"
+    } else if (data == "chipIcon") {
+        webIconImage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+        chipIconImage.style.animation = "iconDown 1.2s";
+        chipIconImage.style.backgroundPositionY = "4.25vw"
+        hoverBubbleChip.style.display = "block";
+        hoverBubbleChip.style.animation = "bubbleAppearArtChip 1.2s"
+    } else if (data == "panelIcon") {
+        // close the cam thing just in case it isnt already
+        hoverDivPanel.style.display = "flex"
+        hoverBubbleCam1.style.display = "none";
+        hoverBubbleCam1.style.animation = "";
+        hoverBubbleCam2.style.display = "none";
+        hoverBubbleCam2.style.animation = "";
+        hoverBubbleCam3.style.display = "none";
+        hoverBubbleCam3.style.animation = "";
+        panelIconImage.style.animation = "iconHover 12s Infinite";
+        panelIconImage.style.backgroundPositionY = ""
+        hoverBubblePanel.style.display = "block"
+        hoverBubblePanel.style.animation = "bubbleAppearPanel 1.2s"
     }
 }
 
@@ -33,10 +86,23 @@ function hoverOff(id) {
         hoverBubbleCam2.style.animation = "";
         hoverBubbleCam3.style.display = "none";
         hoverBubbleCam3.style.animation = "";
+
     } else if (data == "hoverDivArt") {
         artIconImage.style.animation = "";
         artIconImage.style.backgroundPositionY = "2vw"
         hoverBubbleArt.style.display = "none";
         hoverBubbleArt.style.animation = "";
+
+    } else if (data == "hoverDivChip") {
+        chipIconImage.style.animation = "";
+        chipIconImage.style.backgroundPositionY = "2vw"
+        hoverBubbleChip.style.display = "none"
+        hoverBubbleChip.style.animation = "";
+
+    } else if (data == "hoverDivPanel") {
+        panelIconImage.style.animation = "";
+        panelIconImage.style.backgroundPositionY = "2vw"
+        hoverBubblePanel.style.display = "none"
+        hoverBubblePanel.style.animation = "";
     }
 }
