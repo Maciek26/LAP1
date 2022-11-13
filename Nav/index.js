6c1bbde0fc4c247829348e919a04d9911024a7b1
Mace <mawikierszka@gmail.com>
2022-11-13 20:36:58 +0100

    added about & fixed path in NAV

--- Nav/index.js
+++ Nav/index.js
@@ -12,8 +12,8 @@ document.write(`
     </div>
     <div class="navHome" id="navHome" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="http://mace-center.me/LAP1/">Home</a></div>
     <div class="navAbout" id="navAbout" onmouseover="onhover(id)"  ><a style="text-decoration: none; color: antiquewhite" href="http://mace-center.me/LAP1/projects/">Projekte</a></div>
-    <div class="navTemp1" id="navTemp1" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="../#">Ausflüge</a></div>
-    <div class="navTemp2" id="navTemp2" onmouseover="onhover(id)"><a style="text-decoration: none; color: antiquewhite" href="../#">About</a></div>
+    <div class="navTemp1" id="navTemp1" onmouseover="onhover(id)" ><a style="text-decoration: none; color: antiquewhite" href="http://mace-center.me/LAP1/Ausfluege/">Ausflüge</a></div>
+    <div class="navTemp2" id="navTemp2" onmouseover="onhover(id)"><a style="text-decoration: none; color: antiquewhite" href="http://mace-center.me/LAP1/About/">About</a></div>
 
 </div>
 </body>
