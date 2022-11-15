7ca376c0aebea30472de3441a79cecf1fa66d978
Mace <mawikierszka@gmail.com>
2022-11-15 17:37:20 +0100

    debugging & adding mobile device support

--- projects/index.js
+++ projects/index.js
@@ -37,43 +37,53 @@ function scrollDown(id){
     //}
 }
 
+function displayNone(){
+    hoverBubbleCam1.style.display = "none";
+    hoverBubbleCam1.style.animation = "";
+    hoverBubbleCam2.style.display = "none";
+    hoverBubbleCam2.style.animation = "";
+    hoverBubbleCam3.style.display = "none";
+    hoverBubbleCam3.style.animation = "";
+    hoverBubbleArt.style.display = "none";
+    hoverBubbleArt.style.animation = "";
+    hoverBubbleChip.style.display = "none";
+    hoverBubbleChip.style.animation = "";
+    hoverBubblePanel.style.display = "none";
+    hoverBubblePanel.style.animation = "";
+    panelIconImage.style.backgroundPositionY = "2vw"
+}
+
 function hoverDiv(id) {
     var data = id
     if (data == "camIcon") {
+        displayNone()
         webIconImage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
         panelIconImage.style.animation = "";
-        panelIconImage.style.backgroundPositionY = "2vw"
-        hoverBubblePanel.style.display = "none"
-        hoverBubblePanel.style.animation = "";
         hoverBubbleCam1.style.display = "block";
         hoverBubbleCam1.style.animation = "bubbleAppear 1.2s"
         hoverBubbleCam2.style.display = "block";
         hoverBubbleCam2.style.animation = "bubbleAppear2 1.2s"
         hoverBubbleCam3.style.display = "block";
         hoverBubbleCam3.style.animation = "bubbleAppear3 1.2s"
-        hoverDivPanel.style.display = "none"
         hoverBubbleCam3.style.zIndex = "20"
     } else if (data == "artIcon") {
+        displayNone()
         webIconImage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
         artIconImage.style.animation = "iconDown 1.2s";
         artIconImage.style.backgroundPositionY = "4.25vw"
         hoverBubbleArt.style.display = "block";
         hoverBubbleArt.style.animation = "bubbleAppearArtChip 1.2s"
     } else if (data == "chipIcon") {
+        displayNone()
         webIconImage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
         chipIconImage.style.animation = "iconDown 1.2s";
         chipIconImage.style.backgroundPositionY = "4.25vw"
         hoverBubbleChip.style.display = "block";
         hoverBubbleChip.style.animation = "bubbleAppearArtChip 1.2s"
     } else if (data == "panelIcon") {
-        // close the cam thing just in case it isnt already
+        // close the other bubbles just in case it isnt already
+        displayNone()
         hoverDivPanel.style.display = "flex"
-        hoverBubbleCam1.style.display = "none";
-        hoverBubbleCam1.style.animation = "";
-        hoverBubbleCam2.style.display = "none";
-        hoverBubbleCam2.style.animation = "";
-        hoverBubbleCam3.style.display = "none";
-        hoverBubbleCam3.style.animation = "";
         panelIconImage.style.animation = "iconHover 12s Infinite";
         panelIconImage.style.backgroundPositionY = ""
         hoverBubblePanel.style.display = "block"
@@ -110,54 +120,3 @@ function hoverOff(id) {
         hoverBubblePanel.style.animation = "";
     }
 }
-const videoID = "IBqUfixaLS8"
-const thumbnailGian = document.getElementById("thumbnailGian")
-    thumbnailGian.addEventListener("click", function (){
-    groupGian.removeChild(thumbnailGian)
-        const responsiveVideoContainer = document.createElement("div");
-        responsiveVideoContainer.classList.add(
-            "embed-responsive",
-            "embed-responsive-16by9"
-        );
-        const videoIframe = document.createElement("iframe");
-
-        videoIframe.classList.add("embed-responsive-item");
-
-        videoIframe.src = `https://www.youtube.com/embed/${videoID}?autoplay=1&mute=1`;
-
-        videoIframe.setAttribute("allow", "autoplay");
-
-        responsiveVideoContainer.appendChild(videoIframe);
-
-        thumbnailGian.appendChild(responsiveVideoContainer);
-})
-
-//
-// var displayFrame = false
-// var load = window.frames[0].stop()
-//
-// function displayWebsite(){
-//     var buttonLoad = document.getElementById("buttonLoad")
-//     var closeButton = document.getElementById("closeButton")
-//     var linkOpen = document.getElementById("linkOpen")
-//     linkOpen.style.display = "flex"
-//     buttonLoad.style.display = "flex"
-//     closeButton.style.display = "none"
-//     if (displayFrame == false) {
-//         linkOpen.style.display = "none"
-//         buttonLoad.style.display = "none"
-//         closeButton.style.display = "flex"
-//         displayFrame = true
-//         console.log("nope")
-//         document.getElementById("frameWeb").innerHTML = "<iframe src='https://emilewaech.wixsite.com/studienheim' style='z-index:1; display: flex;width: 100vw; height: 50vw; margin-top: 5vw;'>"
-//         groupEmile.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
-//         load = window.frames[30]
-//     } else if (displayFrame == true) {
-//         linkOpen.style.display = "flex"
-//         buttonLoad.style.display = "flex"
-//         closeButton.style.display = "none"
-//         displayFrame = false
-//         console.log("yep")
-//         document.getElementById("frameWeb").innerHTML = ""
-//     }
-// }
